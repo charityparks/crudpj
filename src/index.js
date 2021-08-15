e30ecd2f0a1abe44613fd24adcff6ed6f47817d0
charityparks <tuppercharity@gmail.com>
2021-08-14 20:03:40 -0400

    added firebase config

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as firebase from 'firebase';
 
+var firebaseConfig = {
+  apiKey: "AIzaSyDv9RJv9e3wyI3yFMLjZWCTD4pYc4VKiLE",
+  authDomain: "crudpj-a3baa.firebaseapp.com",
+  projectId: "crudpj-a3baa",
+  storageBucket: "crudpj-a3baa.appspot.com",
+  messagingSenderId: "507750211121",
+  appId: "1:507750211121:web:48e4d591f1fd389d023f10"
+};
+firebase.initializeApp(config);
 ReactDOM.render(
   <React.StrictMode>
     <App />
