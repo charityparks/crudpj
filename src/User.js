fd5bf380be10795ec5f45cb9f2f4487a15ed3a18
charityparks <tuppercharity@gmail.com>
2021-08-28 14:18:03 -0400

    set state

--- src/User.js
+++ src/User.js
@@ -11,8 +11,16 @@ class User extends Component {
    componentDidMount(){
        firebase.database().ref('/')
        .on('value',snapshot => {
-           console.log(snapshot.val())
-       });
+           let returnArr = [];
+           snapshot.forEach(data => {
+               var user =data.val();
+               user['key'] = data.key;
+               returnArr.push(user);
+           });
+           this.setState({
+               users: returnArr
+           })
+        });
    }
    render(){
        return (
