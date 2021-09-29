67880d76468cd6f66905b60a192de0239d5d7646
charityparks <tuppercharity@gmail.com>
2021-09-29 15:31:55 -0400

    syntax errors fixed in user

--- src/User.js
+++ src/User.js
@@ -1,100 +1,101 @@
 import React, { Component } from 'react';
-import firebase from 'firebase/app';
-import { Table, Button, Modal } from 'react-bootstrap';
+import * as firebase from 'firebase';
+import { Table, Button, Modal  } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 class User extends Component {
-   constructor(props) {
-       super(props);
-       this.state = {
-           users: [],
-           showDeleteDialog: false,
-           selectedUser: {}
-       };
-       this.add = this.add.bind(this);
-       this.closeDeleteDialog = this.closeDeleteDialog.bind(this);
-       this.delete = this.delete.bind(this);
 
-   } 
-   componentDidMount(){
-       firebase.database().ref('/')
-       .on('value',snapshot => {
-           let returnArr = [];
-           snapshot.forEach(data => {
-               var user =data.val();
-               user['key'] = data.key;
-               returnArr.push(user);
-           });
-           this.setState({
-               users: returnArr
-           })
-        });
-   }
-   add(e) {
-    this.props.history.push("/add");
-}
+  constructor(props){
+    super(props);        
+    this.state = {
+        users: [],
+        showDeleteDialog: false,
+        selectedUser: {}   
+    };    
+    this.add = this.add.bind(this); 
+    this.closeDeleteDialog = this.closeDeleteDialog.bind(this);                                       
+    this.delete = this.delete.bind(this);        
 
-    openDeleteDialog(user){
+  }  
+
+  componentDidMount(){
+    firebase.database().ref('/')
+      .on('value',snapshot => {
+        let returnArr = [];        
+        snapshot.forEach(data => {
+            var user = data.val();
+            user['key'] = data.key;
+            returnArr.push(user);        
+        });                       
         this.setState({
-            showDeleteDialog: true,
-            selectedUser: user
-        });
-    }
-
-    delete(e) {    
-      firebase.database().ref('/'+this.state.selectedUser.key).remove()
-      .then( x=> {
-          console.log("SUCCESS");
-          this.closeDeleteDialog();
-      })
-      .catch( error => {
-          alert("Could not delete the user.");
-          console.log("ERROR", error)
-      });
-    }  
+            users: returnArr
+        })                
+      });           
+  }
+ 
+  add(e) {
+    this.props.history.push("/add");  
+  }    
 
 
-    closeDeleteDialog() {
-      this.setState({ 
-          showDeleteDialog: false,
-          selectedUser: {}
-      });
-    }
+  openDeleteDialog(user){    
+    this.setState({        
+        showDeleteDialog: true,
+        selectedUser: user
+    });   
+  } 
+  delete(e) {    
+    firebase.database().ref('/'+this.state.selectedUser.key).remove()
+    .then( x=> {
+        console.log("SUCCESS");
+        this.closeDeleteDialog();
+    })
+    .catch( error => {
+        alert("Could not delete the user.");
+        console.log("ERROR", error)
+    });
+  }  
 
-   render(){
+  
+  closeDeleteDialog() {
+    this.setState({ 
+        showDeleteDialog: false,
+        selectedUser: {}
+    });
+  }
+  
+
+  render() {
     const listUsers = this.state.users.map((user) =>   
     <tr key={user.key}>
       <td>{user.username}</td>
       <td>{user.email}</td>
       <td>
-        <Link to={`/edit/${user.key}`}>
-          Edit
-        </Link>
-      </td>
           <Link to={`/edit/${user.key}`}>            
-          </Link>       
-                     
+              Edit
+          </Link>        
+      </td>               
       <td><Button onClick={ this.openDeleteDialog.bind(this,user)}>Remove</Button></td>          
     </tr>                    
-    );    
+    );     
 
-      return (
-        <div>
-            <Button variant="primary" onClick={this.add}>Add</Button>
-            <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>Username</th>
-                    <th>Email</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                  </tr>
-                </thead>
-                    <tbody>
-                        {listUsers}
-                    </tbody>
-            </Table>
-            <Modal show={this.state.showDeleteDialog} onHide={this.closeDeleteDialog}>
+    return (
+      <div> 
+        <Button variant="primary" onClick={this.add}>Add</Button>                           
+        <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUsers}
+        </tbody>
+        </Table> 
+        <Modal show={this.state.showDeleteDialog} onHide={this.closeDeleteDialog}>
           <Modal.Header closeButton>
             <Modal.Title>Delete User</Modal.Title>
           </Modal.Header>
@@ -107,12 +108,12 @@ class User extends Component {
             <Button onClick={this.delete}>Delete</Button>
             <Button onClick={this.closeDeleteDialog}>Close</Button>
           </Modal.Footer>
-        </Modal>     
-        </div>
-       );
-   }
-   
+        </Modal>                          
+
+      </div>
+    );
+  }
+
 }
 
 export default User;
-
