e208f1a117a573c245de8d13c39ab64cd71ab1dd
charityparks <tuppercharity@gmail.com>
2021-09-29 15:34:50 -0400

    cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -2,29 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
-import firebase from 'firebase/app';
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDv9RJv9e3wyI3yFMLjZWCTD4pYc4VKiLE",
-  authDomain: "crudpj-a3baa.firebaseapp.com",
-  projectId: "crudpj-a3baa",
-  storageBucket: "crudpj-a3baa.appspot.com",
-  messagingSenderId: "507750211121",
-  appId: "1:507750211121:web:48e4d591f1fd389d023f10"
-};
-// firebase.initializeApp(config);
-firebase.initializeApp();
+var config = {
+    apiKey: "AIzaSyBN9WlmRc3SedmC4agM1G-rYqezGR22iZE",
+    authDomain: "crudproject-45834.firebaseapp.com",
+    databaseURL: "https://crudproject-45834.firebaseio.com",
+    projectId: "crudproject-45834",
+    storageBucket: "crudproject-45834.appspot.com",
+    messagingSenderId: "590481645308",
+    appId: "1:590481645308:web:613e62539967b876"
+  };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+firebase.initializeApp(config);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
